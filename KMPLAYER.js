3019440438551b8b7335c67ee4ecf29732e2be72
lag <qmariboc@gmail.com>
2024-10-10 18:21:45 -0500

    Add files via upload

---
+++ KMPLAYER.js
@@ -0,0 +1,16 @@
+var video = document.getElementById('video');
+var videoSrc = video.getAttribute('data-src');
+
+if (Hls.isSupported()) {
+    var hls = new Hls();
+    hls.loadSource(videoSrc);
+    hls.attachMedia(video);
+    hls.on(Hls.Events.MANIFEST_PARSED, function() {
+        video.play();
+    });
+} else if (video.canPlayType('application/vnd.apple.mpegurl')) {
+    video.src = videoSrc;
+    video.addEventListener('loadedmetadata', function() {
+        video.play();
+    });
+}
